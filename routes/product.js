18bab41eca7b0270695cb5c9f37c952266de235d
joalmontes <jo.almontes@duocuc.cl>
2024-04-09 11:15:25 -0400

    llamar datos

--- routes/product.js
+++ routes/product.js
@@ -1,9 +1,9 @@
 const express = require ('express')
 const upload = require ('../libs/storage')
-const { addProduct } = require('../controllers/productControllers')
+const { addProduct, getProducts } = require('../controllers/productControllers')
 
 const api = express.Router()
 
 api.post('/products', upload.single('image'), addProduct)
-
+api.get('/products', getProducts)
 module.exports = api
