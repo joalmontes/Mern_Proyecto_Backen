3167e6fde4a8a4107ab86426ddc0f49a9119fd6e
joalmontes <jo.almontes@duocuc.cl>
2024-04-09 10:12:32 -0400

    guardar img

--- routes/product.js
+++ routes/product.js
@@ -1,8 +1,9 @@
 const express = require ('express')
+const upload = require ('../libs/storage')
 const { addProduct } = require('../controllers/productControllers')
 
 const api = express.Router()
 
-api.post('/products', addProduct)
+api.post('/products', upload.single('image'), addProduct)
 
 module.exports = api
